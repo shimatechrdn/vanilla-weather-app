960b300d90d9129d4f6da85f708ae0e3e19999e4
shimatechrdn <127510956+shimatechrdn@users.noreply.github.com>
2023-03-22 20:32:54 +0430

    showing the current temperature

--- src/app.js
+++ src/app.js
@@ -1,1 +1,19 @@
-alert("hello")
+function displayTemperature(response) {
+    console.log(response.data);
+    let temperatureElement= document.querySelector("#temperature");
+    temperatureElement.innerHTML=Math.round(response.data.main.temp);
+    let cityElement= document.querySelector("#cuty");
+    cityElement.innerHTML= response.data.name
+    let descriptionElement= document.querySelector("#description");
+    descriptionElement.innerHTML= response.data.weather[0].description;
+    let humidityElement=document.querySelector("#humidity");
+    humidityElement.innerHTML= response.data.main.humidity;
+    let windElement=document.querySelector("#wind");
+    windElement.innerHTML=Math.round(response.data.wind.speed);
+}
+
+
+let apiKey="8c48afa47a9a9c24f3500c7039d50aaa"
+let apiUrl="https://api.openweathermap.org/data/2.5/weather?q=New%20York&appid=8c48afa47a9a9c24f3500c7039d50aaa&units=metric"
+
+axios.get(apiUrl).then(displayTemperature);
