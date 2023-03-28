4980b82c379751adff98b38111c405e9ad5a1862
shimatechrdn <127510956+shimatechrdn@users.noreply.github.com>
2023-03-29 02:26:45 +0430

    complete js process through manipulating data

--- src/app.js
+++ src/app.js
@@ -33,7 +33,43 @@ function formatDate(timestamp){
     let day=days[date.getDay()];
     return `${day} ${hours}:${minutes}`;
 
+
 }
+
+function displayForecast(){
+    let forecastElement= document.querySelector("#forecast");
+    let forecastHTML= `<div class="row">`;
+    let days= ["Thu","Fri","Sat","Sun"];
+    days.forEach(function (day){
+        forecastHTML= forecastHTML+`<div class="c0l-2">
+                        <div class="weather-forecast-date">
+                            ${day}
+                        </div>
+                        <img src="....." alt="" width="36" />
+                        <div class="weather-forecast-temperature">
+                            <span class="weather-forecast-temperature-max">.
+                                18°
+                            </span>
+                            <span class="weather-forecast-temperature-min">
+                                12°
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>`
+    }
+    )
+
+    
+        forecastHTML= forecastHTML +`</dive>`;
+        forecastElement.innerHTML=forecastHTML;
+}
+
+
+
 function search(city){
 let apiKey="8c48afa47a9a9c24f3500c7039d50aaa"
 let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=8c48afa47a9a9c24f3500c7039d50aaa&units=metric`
@@ -79,4 +115,6 @@ let celsiusLink=document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click",displayCelsiusTemperature);
 
 search("New York");
+
+displayForecast();
 
